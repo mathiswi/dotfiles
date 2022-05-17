6afde9317b53e9f7e5770f0c6583079a68a3149d
Mathis Witte <mail@mwitte.dev>
2022-05-17 02:25:29 +0200

    Update config

--- link_config.sh
+++ link_config.sh
@@ -1,17 +1,22 @@
 
 link_config() {
   echo -e $'\n### Linking Config ###\n'
-  rm ~/.zshrc
+  rm -rf /~/.config/i3
   ln -sv ~/dotfiles/i3 ~/.config/
+  rm -rf /~/.config/colorschemes
   ln -sv ~/dotfiles/colorschemes ~/.config/
+  rm -rf /~/.config/polybar
   ln -sv ~/dotfiles/polybar ~/.config/
+  rm -rf /~/.config/rofi
   ln -sv ~/dotfiles/rofi ~/.config/
+  rm -rf /~/.config/wallpaper
   ln -sv ~/dotfiles/wallpaper ~/.config/
+  rm /~/.config/greenclip.toml
   ln -sv ~/dotfiles/greenclip.toml ~/.config/
-  ln -sv ~/dotfiles/picom.conf ~/.config/
   ln -sv ~/dotfiles/.Xresources ~/.config/
+  rm -rf /~/.config/dunst
   ln -sv ~/dotfiles/dunst ~/.config/
-  ln -sv ~/dotfiles/.xbindkeysrc ~/
+  rm ~/.zshrc
   ln -sv ~/dotfiles/.zshrc ~/
 }
 
